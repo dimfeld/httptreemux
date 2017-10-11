1b3bbda556956c28b21bfadbc0103f7a117a7a9a
Scott Johnson <7.scott.j@gmail.com>
2017-10-11 15:31:09 -0600

    make url.PathUnescape backwards compatable

---
+++ tree_18.go
@@ -0,0 +1,11 @@
+// +build go1.8
+
+package httptreemux
+
+//Break out unescapePath to use url.PathUnescape in go 1.8+
+
+import "net/url"
+
+func unescapePath(path string) (string, error) {
+	return url.PathUnescape(path)
+}
